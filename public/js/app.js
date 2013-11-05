7d2aa6dc07706052a042b59ab51001104d48fd4f
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2013-11-05 18:54:48 +0100

    Adds nvd3 graph example from http://nvd3.org/ghpages/line.html

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-console.log('app load');
 var app = angular.module('leanmean', []);
 
 app.config(function ($routeProvider, $locationProvider) {
@@ -12,25 +11,73 @@ app.config(function ($routeProvider, $locationProvider) {
   $routeProvider
 
     .when('/', {
-      controller:  OneController,
       templateUrl: 'partials/one'
     })
 
     .when('/one', {
-      controller:  OneController,
       templateUrl: 'partials/one'
     })
 
     .when('/two', {
-      controller:  TwoController,
+      controller: TwoController,
       templateUrl: 'partials/two'
     })
 
 });
 
-function OneController () {
-  console.log('OneController');
+function TabsController ($scope, $location) {
+  $scope.oneActive = $location.path() == '/one';
+  $scope.twoActive = $location.path() == '/two';
 }
+
 function TwoController () {
-  console.log('TwoController');
+
+   nv.addGraph(function() {
+   var chart = nv.models.lineChart();
+
+   chart.xAxis
+       .axisLabel('Time (ms)')
+       .tickFormat(d3.format(',r'));
+
+   chart.yAxis
+       .axisLabel('Voltage (v)')
+       .tickFormat(d3.format('.02f'));
+
+   d3.select('.tabs-two svg')
+       .datum(sinAndCos())
+     .transition().duration(500)
+       .call(chart);
+
+   nv.utils.windowResize(function() { d3.select('.tabs-two svg').call(chart) });
+
+   return chart;
+ });
+
+ /**************************************
+  * Simple test data generator
+  */
+
+
+ function sinAndCos() {
+   var sin = [],
+       cos = [];
+
+   for (var i = 0; i < 100; i++) {
+     sin.push({x: i, y: Math.sin(i/10)});
+     cos.push({x: i, y: .5 * Math.cos(i/10)});
+   }
+
+   return [
+     {
+       values: sin,
+       key: 'Sine Wave',
+       color: '#ff7f0e'
+     },
+     {
+       values: cos,
+       key: 'Cosine Wave',
+       color: '#2ca02c'
+     }
+   ];
+ }
 }

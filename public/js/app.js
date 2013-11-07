00f01f30fd1e7ad70dcc82487fb7fdf1ac58e233
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2013-11-07 19:44:43 +0100

    Restructure load to init app earlier.

--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,4 @@
-var Routes = {
-  one: 'one.html',
-  two: 'two.html'
-};
-
 var app = angular.module('leanmean', []);
-
-app.value('Routes', Routes);
-
-app.config(function ($routeProvider, $locationProvider) {
-
-  $locationProvider.html5Mode(true);
-
-  //$rootScope.$on('$afterRouteChange', function(){
-    //$window.scrollTo(0,0);
-  //});
-
-  $routeProvider
-
-    .when('/', {
-      controller: OneController,
-      templateUrl: 'partials/' + Routes.one
-    })
-
-    .when('/' + Routes.one, {
-      controller: OneController,
-      templateUrl: 'partials/' + Routes.one
-    })
-
-    .when('/' + Routes.two, {
-      controller: TwoController,
-      templateUrl: 'partials/' + Routes.two
-    })
-
-});
 
 // hack when in subfolder, angular seems to need this
 var subdir = '/fagdag-elasticsearch/';
